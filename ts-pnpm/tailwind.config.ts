7e6b46d267c55b6df4dfd74809e707b91eb3ea0a
corex303 <you17c@ncssm.edu>
2025-06-06 11:06:45 -0400

    content paths in tailwind.config.ts

--- ts-pnpm/tailwind.config.ts
+++ ts-pnpm/tailwind.config.ts
@@ -1,8 +1,12 @@
 import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
     darkMode: ['class'],
     content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/layout/Header.tsx',
     './src/components/layout/Footer.tsx',
