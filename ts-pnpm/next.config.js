5d2411e93b9e7b4011ba1eea16d16c20bf49cb85
corex303 <you17c@ncssm.edu>
2025-06-06 11:21:34 -0400

    Remove experimental flag

--- ts-pnpm/next.config.js
+++ ts-pnpm/next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -59,6 +61,11 @@ const nextConfig = {
 
     // Modify the file loader rule to ignore *.svg, since we have it handled now.
     fileLoaderRule.exclude = /\.svg$/i;
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
 
     return config;
   },
